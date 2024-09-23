21d2856a5eb2958a63e2844834cb305f30d87af7
Stiveys <stevomwaura@gmail.com>
2024-09-23 13:59:31 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Steve Baars!';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)}. Baars!`;
